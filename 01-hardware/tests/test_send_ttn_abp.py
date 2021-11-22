7877a9a2b87cf87b598c28ef5926d803d3890519
gwongwen <gwongwen.trash@gmail.com>
2021-11-22 10:49:03 +0100

    add SX127x driver and improve code

--- 01-hardware/tests/test_send_ttn_abp.py
+++ 01-hardware/tests/test_send_ttn_abp.py
@@ -2,73 +2,48 @@
 # test: send physical data from sensor to LoRaWAN TTN by ABP identification
 # version 1.0 - 19/11/21
 
-import time, busio, board, adafruit_ssd1306, adafruit_bmp3xx, sys, random
+import sys,adafruit_ssd1306,adafruit_bmp3xx,board
 from digitalio import DigitalInOut, Direction, Pull
-from adafruit_tinylora.adafruit_tinylora import TTN, TinyLoRa
-from pathlib import Path
+from time import sleep
+sys.path.insert(0, '/home/pi/solar_pi0_ws_otaa/01-hardware/SX127x')
+from SX127x.LoRa import *
+from SX127x.LoRaArgumentParser import LoRaArgumentParser
+from SX127x.board_config import BOARD
+sys.path.insert(0, '/home/pi/solar_pi0_ws_otaa/04-lorawan')
+from LoRaWAN.MHDR import MHDR
+from random import randrange
 
-# updates the sleep time on the environment and on the 
-# .env.dynamic file
-# PARAMS: 
-# - env-file: name of the file in which update the time value
-# - value: new sleep time value
-# ATENTION: this method will crush the env_file and create another one
-# containing only that line
-#
-# RETURN: None
-def setNewSleepTime(env_file, value):
-    dynamic_env_file = open(env_file, "w")
-    dynamic_env_file.write("export SLEEPTIME = "+str(value))
-    dynamic_env_file.close()
-    os.environ[key] = value
+BOARD.setup()
+parser = LoRaArgumentParser("LoRaWAN sender")
 
-# get the TTN key from the env and pass it to bytearray
-# PARAMS: 
-# - key: key needed (must be on the .env file)
-# RETURN: bytearray key value     
-def getTTNKey(key):
-    try:
-        key_to_retrieve_string = os.getenv(key)
-        key_to_retrieve_bytearray = bytearray.fromhex(key_to_retrieve_string)
-        return key_to_retrieve_bytearray
-    except Exception:
-        print("Error retrieving TTN key")
-        print(sys.exc_info())
-        sys.exit()
+class LoRaWANsend(LoRa):
+    def __init__(self, devaddr = [], nwkey = [], appkey = [], verbose = False):
+        super(LoRaWANsend, self).__init__(verbose)
+        self.devaddr = devaddr
+        self.nwkey = nwkey
+        self.appkey = appkey
 
-# get the TTN country code as an string
-# PARAMS: None
-# RETURN: string country code value 
-def getTTNCountryFromENV():
-    try:
-        country = os.getenv("COUNTRY")
-        return country
-    except Exception:
-        print("Error retrieving TTN country code")
-        print(sys.exc_info())
-        sys.exit()
+    def on_tx_done(self):
+        self.set_mode(MODE.STDBY)
+        self.clear_irq_flags(TxDone=1)
+        print("TxDone")
+        sys.exit(0)
 
-def loadEnv(env_file_name):
-    try:
-        env_path = Path('.') / env_file_name
-        load_dotenv(dotenv_path=env_path)
-    except Exception:
-        print("Couldnt load ENV")
-        print(sys.exc_info())
-        sys.exit()
+    def start(self):
+        lorawan = LoRaWAN.new(nwskey, appskey)
+        lorawan.create(MHDR.UNCONF_DATA_UP, {'devaddr': devaddr, 'fcnt': 1, 'data': list(map(ord, 'Python rules!')) })
 
-# generate fake data body
-# PARAMS: None
-# RETURN: data encoded
+        self.write_payload(lorawan.to_raw())
+        self.set_mode(MODE.TX)
+        while True:
+            sleep(1)
+
 def getPayloadMockBMP388():
     press_val = bmp.pressure
     temp_val = bmp.temperature + temperature_offset
     alt_val = bmp.altitude
     return encodePayload(press_val,temp_val,alt_val)
 
-# encodes payload
-# PARAMS: pressure, temperature, and altitude from BME388 sensor
-# RETURN: data encoded
 def encodePayload(pressure,temperature,altitude):
     # encode float as int
     press_val = int(pressure * 100) 
@@ -97,9 +72,6 @@ def encodePayload(pressure,temperature,altitude):
 
     return data
 
-# send data
-# PARAMS: data
-# RETURN: None
 def sendDataTTN(data):
     lora.send_data(data, len(data), lora.frame_counter)
     lora.frame_counter += 1
@@ -107,21 +79,22 @@ def sendDataTTN(data):
     display.text('Packet sent', 10, 0, 1)
     display.show()
 
-# init env 
-loadEnv('.env')
-loadEnv('.env.dynamic')
+# init
+devaddr = [0x26, 0x01, 0x3D, 0x54]
+nwskey = [0x0F, 0xFE, 0xDF, 0x1D, 0x36, 0x6D, 0x51, 0x89, 0x76, 0xD7, 0x76, 0xBB, 0x92, 0xA5, 0x9A, 0xE9]
+appskey = [ 0x4A, 0xD7, 0xB6, 0x3F, 0x86, 0xAB, 0xC7, 0x54, 0xCF, 0x26, 0x8E, 0xE5, 0x60, 0xDE, 0x1C, 0x99]
+lora = LoRaWANsend(False)
 
-# link buttons
-btnA = DigitalInOut(board.D5) # button A
-btnA.direction = Direction.INPUT
-btnA.pull = Pull.UP
-btnB = DigitalInOut(board.D6) # button B
-btnB.direction = Direction.INPUT
-btnB.pull = Pull.UP
-btnC = DigitalInOut(board.D12) # button C
-btnC.direction = Direction.INPUT
-btnC.pull = Pull.UP
- 
+# setup
+lora.set_mode(MODE.SLEEP)
+lora.set_dio_mapping([1,0,0,0,0,0])
+lora.set_freq(868.1)
+lora.set_pa_config(pa_select=1)
+lora.set_spreading_factor(7)
+lora.set_pa_config(max_power=0x0F, output_power=0x0E)
+lora.set_sync_word(0x34)
+lora.set_rx_crc(True)
+
 # create the i2c interface
 i2c = board.I2C()   # uses board.SCL and board.SDA
  
@@ -134,39 +107,40 @@ bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
 # change this to match the location's pressure (hPa) at sea level
 bmp.sea_level_pressure = 1013.25
-
 bmp.pressure_oversampling = 1
 bmp.temperature_oversampling = 1
-
-# you will usually have to add an offset to account for the temperature of
-# the sensor. This is usually around 5 degrees but varies by use. Use a
-# separate temperature sensor to calibrate this one.
 temperature_offset = -5
+
+# link buttons
+btnA = DigitalInOut(board.D5) # button A
+btnA.direction = Direction.INPUT
+btnA.pull = Pull.UP
+btnB = DigitalInOut(board.D6) # button B
+btnB.direction = Direction.INPUT
+btnB.pull = Pull.UP
+btnC = DigitalInOut(board.D12) # button C
+btnC.direction = Direction.INPUT
+btnC.pull = Pull.UP
 
 # clear the display.
 display.fill(0)
 display.show()
 width = display.width
 height = display.height
- 
-# TinyLoRa configuration
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-cs = DigitalInOut(board.CE1)
-irq = DigitalInOut(board.D22)
-rst = DigitalInOut(board.D25)
 
-# retrieve TTN Keys from environment
-# TTN device Address, 4 Bytes, MSB
-devaddr = getTTNKey("DEVADDR")
-# TTN network Key, 16 Bytes, MSB
-nwkey = getTTNKey("NWKEY")
-# TTN application Key, 16 Bytess, MSB
-app = getTTNKey("APP")
+print(lora)
+assert(lora.get_agc_auto_on() == 1)
 
-# initialize ThingsNetwork configuration
-ttn_config = TTN(devaddr, nwkey, app, country=getTTNCountryFromENV())
-# initialize lora object
-lora = TinyLoRa(spi, cs, irq, rst, ttn_config)
+try:
+    print("Sending LoRaWAN message\n")
+    lora.start()
+except KeyboardInterrupt:
+    sys.stdout.flush()
+    print("\nKeyboardInterrupt")
+finally:
+    sys.stdout.flush()
+    lora.set_mode(MODE.SLEEP)
+    BOARD.teardown()
 
 # 2b array to store sensor data
 data = bytearray(9)
