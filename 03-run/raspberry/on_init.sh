ad0c09d27feb7f0215199cfb4aa0f7f578f55734
gwongwen <gwongwen.trash@gmail.com>
2022-11-18 14:15:18 +0100

    change talkpp RTC clock setting

--- 03-run/raspberry/on_init.sh
+++ 03-run/raspberry/on_init.sh
@@ -28,7 +28,7 @@ TIMELAPSE=1800		# 30 mins
 TIMELOWBATT=600		# 10 mins
 
 # get the date from the RTC
-MYDATE=$(talkpp -s)			# device RTC with the current system clock
+MYDATE=$(talkpp -t)			# device RTC with the current system clock --> test ?
 MYHHMM=${MYDATE:4:4}
 
 echo "setup date from RTC"
