a5d44c5cde586da07a0dd3be46e77346c9f4f214
gwongwen <gwongwen.trash@gmail.com>
2022-12-16 14:12:08 +0100

    add command to disable unnecessary services

--- 05-measure/raspberry/meas_batt.sh
+++ 05-measure/raspberry/meas_batt.sh
@@ -1,7 +1,18 @@
 #! /usr/bin/bash
 # first script test to measure battery discharge time
+# version 1.0 - 16/12/22 (add command to disable unnecessary services)
 
 echo beginning of script
+
+# disable BLE
+sudo rfkill block bluetooth
+
+# disable LED
+#echo 1 > /sys/class/leds/led0/brightness     # turn on
+echo 0 > /sys/class/leds/led0/brightness     # turn off
+
+# disable HDMI output
+sudo /usr/bin/tvservice -o
 
 #MYDATE=$(talkpp -t)
 MYDATE=$(talkpp -s)
@@ -21,4 +32,5 @@ do
 	# 1 measurement every 5 minutes 
 	sleep 300
 done
+
 echo $itr >> batt.txt
