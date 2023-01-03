a752a2192ff31ed2981fc5d629a5aa65b2f910bf
gwongwen <gwongwen.trash@gmail.com>
2023-01-03 10:29:55 +0100

    bug fixe of while test

--- 05-measure/raspberry/meas_batt.sh
+++ 05-measure/raspberry/meas_batt.sh
@@ -28,12 +28,12 @@ itr=1
 BATT=$(talkpp -c B)
 
 # value with talkpp: 3.06 ; value with scope: 3.76 (min value)
-while [[ $BATT>3.05 ]];
+while [ $BATT>3.05 ];
 do
 	echo $itr
 	BATT=$(talkpp -c B)
 	echo $BATT >> batt.txt
-	((itr++))
+	itr=$(($itr + 1))
 	# 1 measurement every 5 minutes 
 	sleep 300
 done
