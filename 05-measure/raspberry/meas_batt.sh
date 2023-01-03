371c3c18174a5039f234c70a0d76c7b63a29b974
gwongwen <gwongwen.trash@gmail.com>
2023-01-03 14:52:45 +0100

    problem WARN of battery value in talkpp command

--- 05-measure/raspberry/meas_batt.sh
+++ 05-measure/raspberry/meas_batt.sh
@@ -25,17 +25,11 @@ MYDATE=$(talkpp -f)
 echo $MYDATE >> batt.txt
 
 itr=1
-BATT=$(talkpp -c B)
-
-# value with talkpp: 3.06 ; value with scope: 3.76 (min value)
-while [ $BATT>3.05 ];
+while [ itr -le 200]:
 do
-	echo $itr
 	BATT=$(talkpp -c B)
 	echo $BATT >> batt.txt
-	itr=$(($itr + 1))
+	((itr=itr+1))
 	# 1 measurement every 5 minutes 
 	sleep 300
 done
-
-echo $itr >> batt.txt
